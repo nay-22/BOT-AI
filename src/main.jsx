20afb3d464b7b52e937a7a03872c8af7095dced5
Nayan <nayan92001@gmail.com>
2024-08-24 05:22:16 +0530

    Added Notistack SnackbarProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SnackbarProvider } from 'notistack'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </StrictMode>,
 )

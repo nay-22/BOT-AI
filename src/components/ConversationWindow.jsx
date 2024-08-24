cb333e4c7981708badd6f3bb2b113d0dbe18a9ad
Nayan <nayan92001@gmail.com>
2024-08-25 05:01:49 +0530

    Added rating attributes to Message component

--- src/components/ConversationWindow.jsx
+++ src/components/ConversationWindow.jsx
@@ -3,8 +3,13 @@ import User from "../assets/user.png";
 import Logo from "../assets/logo.png";
 import { Grow, LinearProgress, List, ListItem } from "@mui/material";
 import { TransitionGroup } from "react-transition-group";
+import { useContext, useState } from "react";
+import ConversationContext from "../contexts/ConversationContext";
 
-const ConversationWindow = ({ conversations }) => {
+const ConversationWindow = ({}) => {
+
+    const {conversations} = useContext(ConversationContext);
+
     return (
         <List>
             <TransitionGroup>
@@ -12,10 +17,12 @@ const ConversationWindow = ({ conversations }) => {
                     <Grow key={index} timeout={500}>
                         <ListItem >
                             <Message
+                                id={message.id}
                                 message={message.message}
                                 username={message.username}
                                 timestamp={message.time}
                                 roleIcon={message.username === 'You' ? User : Logo}
+                                rating={message.username === 'You' ? undefined : message.rating}
                             />
                         </ListItem>
                     </Grow>

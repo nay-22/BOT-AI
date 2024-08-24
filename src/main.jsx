6d065e216fa3e5dc905424911656c5a4381b7900
Nayan <nayan92001@gmail.com>
2024-08-25 05:03:36 +0530

    Added Browser Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { SnackbarProvider } from 'notistack'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SnackbarProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </SnackbarProvider>
   </StrictMode>,
 )
